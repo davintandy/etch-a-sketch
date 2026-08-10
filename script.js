bc66133f6c0d6947ac3cd684c05e11ec8eb4d985
Davin Tandy <dav.tandy@gmail.com>
2026-08-10 23:15:51 +0700

    Set up a mouse hover effect

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const container = document.getElementById("container");
 for (let i = 0; i < 16*16; i++) {
     const newDiv = document.createElement("div");
     newDiv.className = "square";
+    newDiv.addEventListener("mouseover", () => {
+        newDiv.style.backgroundColor = "black";
+    })
     container.appendChild(newDiv);
 }
