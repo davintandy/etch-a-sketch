ef35f4c81d68a61d85ab45fc35dcea96a4bbb86c
Davin Tandy <dav.tandy@gmail.com>
2026-08-22 09:32:19 +0700

    Create a function to generate custom grid size based on user input

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 const container = document.getElementById("container");
+const button = document.getElementById("grid");
 
-for (let i = 0; i < 16*16; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.className = "square";
-    newDiv.addEventListener("mouseover", () => {
-        newDiv.style.backgroundColor = "black";
-    })
-    container.appendChild(newDiv);
+createGrid(16)
+
+button.addEventListener("click", function() {
+    let gridSize = prompt("Set Grid Size (1-100)", 16);
+
+    gridSize = parseInt(gridSize);
+    
+    if (isNaN(gridSize) || gridSize < 1) {
+        gridSize = 1;
+    }
+
+    if (gridSize > 100) {
+        gridSize = 100;
+    }
+
+    container.style.setProperty("--grid-size", gridSize);
+
+    createGrid(gridSize);
+})
+
+function createGrid(size) {
+    container.innerHTML = "";
+
+    const totalSquares = size * size;
+
+    for (let i = 0; i < totalSquares; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.className = "square";
+        newDiv.addEventListener("mouseover", () => {
+            newDiv.style.backgroundColor = "black";
+        })
+        container.appendChild(newDiv);
+    }
 }
